f3ff2aea556dde6ed1e2ede3eadce7c3236e5a11
Eshan Lee <ele54@uclive.ac.nz>
2024-04-20 02:52:06 +1200

    CB status trips based on signal

--- prototype1/prototype1.ino
+++ prototype1/prototype1.ino
@@ -1,15 +1,29 @@
 /*
 Prototype 1:
 
-  CB_status is outputted etc.
+  CB status trips based on signal
 */
+
+#define trip_signal 2
+#define CB_output 3
+
+bool CB_status = 1;  //0 for open/tripped (1 for closed/untripped)
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  //input pins
+  pinMode(trip_signal, INPUT);   
 
+  //output pins
+  pinMode(CB_output, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (trip_signal == HIGH) {
+    CB_status = 0;
+  }
 
+  digitalWrite(CB_output, CB_status);
 }
