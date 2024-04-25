c10d3a64c791a7bf754ab6145aa0f2e5f57ecc89
Eshan Lee <ele54@uclive.ac.nz>
2024-04-26 10:01:24 +1200

    add ability to react to auto reclose signal

--- prototype1/prototype1.ino
+++ prototype1/prototype1.ino
@@ -2,10 +2,12 @@
 Prototype 1:
 
   CB status trips based on signal
+  CB recloses based on auto reclose signal
 */
 
-#define trip_signal 2
+#define trip_input 2
 #define CB_output 3
+#define auto_reclose_signal 4
 
 bool CB_status = 1;  //0 for open/tripped (1 for closed/untripped)
 
@@ -13,16 +15,23 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   //input pins
-  pinMode(trip_signal, INPUT);   
+  pinMode(trip_input, INPUT);   
+  pinMode(auto_reclose_input, INPUT);   
 
   //output pins
   pinMode(CB_output, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  trip_signal = digitalRead(trip_input);
+  auto_reclose_signal = digitalRead(auto_reclose_input);
+  
   if (trip_signal == HIGH) {
     CB_status = 0;
+  }
+
+  if (auto_reclose_signal == HIGH) {
+    CB_status = 1;
   }
 
   digitalWrite(CB_output, CB_status);
