ea1a544a5647cdb8eefe56922b25d3fd80b8b9ba
Eshan Lee <ele54@uclive.ac.nz>
2024-04-26 12:08:34 +1200

    add spring charged state output (not toggled)

--- prototype1/prototype1.ino
+++ prototype1/prototype1.ino
@@ -1,15 +1,23 @@
 /*
 Prototype 1:
 
-  CB status trips based on signal
-  CB recloses based on auto reclose signal
+  CB status trips based on signal.
+  CB recloses based on auto reclose signal.
 */
 
+// I/O pins
+// can use digital pins 2 to 19
 #define trip_input 2
 #define CB_output 3
 #define auto_reclose_signal 4
+#define spring_charged_state_output 5
+// #define CB_control_sw 6
+// auxiliary outputs
 
+// status variables
 bool CB_status = 1;  //0 for open/tripped (1 for closed/untripped)
+bool spring_charged_state = 1; //1 for charged, 0 for not charged. !!NEED SOMETHING TO TOGGLE THIS
+// auxiliary contacts
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,9 +25,11 @@ void setup() {
   //input pins
   pinMode(trip_input, INPUT);   
   pinMode(auto_reclose_input, INPUT);   
+  // pinMode(CB_control_sw, INPUT);   
 
   //output pins
   pinMode(CB_output, OUTPUT);
+  pinMode(spring_charged_state_output, OUTPUT);
 }
 
 void loop() {
@@ -35,4 +45,5 @@ void loop() {
   }
 
   digitalWrite(CB_output, CB_status);
+  digitalWrite(spring_charged_state, spring_charged_state_output);
 }
