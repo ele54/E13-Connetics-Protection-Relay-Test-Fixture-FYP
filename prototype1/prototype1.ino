c9e9a7fbc8520ed8951b0df41ed42c0f591a5747
Eshan Lee <ele54@uclive.ac.nz>
2024-05-03 12:34:45 +1200

    Remove autoreclose code, add spring-charge status functionality

--- prototype1/prototype1.ino
+++ prototype1/prototype1.ino
@@ -9,8 +9,8 @@ Prototype 1:
 // can use digital pins 2 to 19
 #define trip_input 2
 #define CB_output 3
-#define auto_reclose_signal 4
-#define spring_charged_state_output 5
+#define spring_charged_contact_input 4
+#define spring_charged_contact_output 5
 // #define auxiliary_1_output 6
 // #define auxiliary_2_output 6
 // #define CB_control_sw 6
@@ -18,39 +18,40 @@ Prototype 1:
 
 // status variables
 bool CB_status = 1;  //0 for open/tripped (1 for closed/untripped)
-bool spring_charged_state = 1; //1 for charged, 0 for not charged. !!NEED SOMETHING TO TOGGLE THIS
+bool spring_charged_switch = 1; //1 for charged (switch closed), 0 for not charged (switch open). !!NEED SOMETHING TO TOGGLE THIS
 // auxiliary contacts
 
-struct auxiliary {
-  int output_pin;
-  bool status;
-}
+// struct auxiliary {
+//   int output_pin;
+//   bool status;
+// }
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   //input pins
   pinMode(trip_input, INPUT);   
-  pinMode(auto_reclose_input, INPUT);   
+  pinMode(spring_charged_contact_input, INPUT);   
   // pinMode(CB_control_sw, INPUT);   
 
   //output pins
   pinMode(CB_output, OUTPUT);
-  pinMode(spring_charged_state_output, OUTPUT);
+  pinMode(spring_charged_contact_output, OUTPUT);
 }
 
 void loop() {
-  trip_signal = digitalRead(trip_input);
-  auto_reclose_signal = digitalRead(auto_reclose_input);
+  int trip_signal = digitalRead(trip_input);
   
   if (trip_signal == HIGH) {
     CB_status = 0;
   }
 
-  if (auto_reclose_signal == HIGH) {
-    CB_status = 1;
+  if (spring_charged_switch == 1) {
+    int spring_charge_status = digitalRead(spring_charged_contact_input);
+    digitalWrite(spring_charged_contact_output, spring_charge_status);
+  } else {
+    digitalWrite(spring_charged_contact_output, 0);   // pulls spring-charged switch low if it is open
   }
 
   digitalWrite(CB_output, CB_status);
-  digitalWrite(spring_charged_state, spring_charged_state_output);
 }
