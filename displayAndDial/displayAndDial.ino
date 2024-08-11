ed3ced7fee9e3619a788fcc1e604a6ab2bd2bbc1
Eshan Lee <ele54@uclive.ac.nz>
2024-08-11 18:08:07 +1200

    fix compile errors

--- displayAndDial/displayAndDial.ino
+++ displayAndDial/displayAndDial.ino
@@ -10,18 +10,18 @@ Reads a dial and outputs the value to a 3 digit seven segment display
 #define SEG_CLOCK_PIN 8
 #define SEG_DATA_PIN 9
 int pot_value;
-const DisplayType DISPLAY_TYPE = COMMON_CATHODE;
-#define DISPLAY_SIZE = 3;
+const int DISPLAY_TYPE = COMMON_CATHODE;
+#define DISPLAY_SIZE 3
 
 ShiftDisplay display(SEG_LATCH_PIN, SEG_CLOCK_PIN, SEG_DATA_PIN, DISPLAY_TYPE, DISPLAY_SIZE);
 
 void setup() {
-    pinmode(POT_PIN, INPUT);
+    pinMode(POT_PIN, INPUT);
 }
 
 void loop() {
     pot_value = analogRead(POT_PIN);
     // pot_value = map(value, 0, 1023, 0, 255);//map value
     display.set(pot_value);
-    display.update();
+    display.show();
 }
