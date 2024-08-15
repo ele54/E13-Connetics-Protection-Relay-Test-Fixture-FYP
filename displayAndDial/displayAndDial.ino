9ab9a334e7a30e5a72dcdb52c212784dece15537
Eshan Lee <ele54@uclive.ac.nz>
2024-08-16 00:30:16 +1200

    fix adc value mapping and display 2 d.p.

--- displayAndDial/displayAndDial.ino
+++ displayAndDial/displayAndDial.ino
@@ -9,20 +9,20 @@ Reads a dial and outputs the value to a 3 digit seven segment display
 #define SEG_LATCH_PIN 7
 #define SEG_CLOCK_PIN 8
 #define SEG_DATA_PIN 9
-int pot_value;
+float pot_value;
 const int DISPLAY_TYPE = COMMON_CATHODE;
 #define DISPLAY_SIZE 3
 
 ShiftDisplay display(SEG_LATCH_PIN, SEG_CLOCK_PIN, SEG_DATA_PIN, DISPLAY_TYPE, DISPLAY_SIZE);
 
 void setup() {
-    pinMode(POT_PIN, INPUT);
+  Serial.begin(9600);
+  pinMode(POT_PIN, INPUT);
 }
 
 void loop() {
-    pot_value = analogRead(POT_PIN);
-    pot_value = map(value, 0, 1023, 0, 200);//map value
-    display.set(pot_value);
-    display.setDot(1, true);
-    display.show();
+  pot_value = analogRead(POT_PIN);
+  pot_value = pot_value/1023 * 2;   //map adc value to 0-2A
+  display.set(pot_value, 2, 0);
+  display.show();
 }
