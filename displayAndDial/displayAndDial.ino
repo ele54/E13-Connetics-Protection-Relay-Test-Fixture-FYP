3aa0a798e8bfa711b2e7306b07234dd83cc28bd7
Eshan Lee <ele54@uclive.ac.nz>
2024-10-10 22:42:09 +1300

    remove unuseful files and update displayAndDial header

--- displayAndDial/displayAndDial.ino
+++ displayAndDial/displayAndDial.ino
@@ -1,14 +1,16 @@
 /*
-User interface for the analogue current function:
-Reads a dial and outputs the value to a 3 digit seven segment display
+
+Code for testing the user interface for the AC current source:
+  - reads the dial 
+  - outputs the value to the 3 digit seven segment display
 
 */
 #include <ShiftDisplay.h>
 
 #define POT_PIN A2
+#define SEG_DATA_PIN 6
 #define SEG_LATCH_PIN 7
 #define SEG_CLOCK_PIN 8
-#define SEG_DATA_PIN 9
 float pot_value;
 const int DISPLAY_TYPE = COMMON_CATHODE;
 #define DISPLAY_SIZE 3
@@ -16,7 +18,6 @@ const int DISPLAY_TYPE = COMMON_CATHODE;
 ShiftDisplay display(SEG_LATCH_PIN, SEG_CLOCK_PIN, SEG_DATA_PIN, DISPLAY_TYPE, DISPLAY_SIZE);
 
 void setup() {
-  Serial.begin(9600);
   pinMode(POT_PIN, INPUT);
 } 
 
